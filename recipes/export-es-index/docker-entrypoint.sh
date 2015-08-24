0d06e9b3ba63fb5e67c799fa587df696973ea423
Tristan Carel <tristan@cogniteev.com>
2015-08-24 11:08:48 +0200

    New Docker recipe to export an Elasticsearch index

---
+++ recipes/export-es-index/docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -e
+
+if [ "xES_INDEX" = x ] ;then
+  echo >&2 <<EOF
+Error: Elasticsearch index to export is not specifie.
+Expecting ES_INDEX environment variable.
+Abort.
+EOF
+  exit 1
+fi
+
+if [ "x${ELASTICSEARCH_PORT_9200_TCP_PORT}" = x ] ; then
+  echo >&2 "Error: elasticsearch container does not seem to be linked."
+  exit 1
+fi
+
+ES_URL="http://elasticsearch:${ELASTICSEARCH_PORT_9200_TCP_PORT}"
+
+elasticdump                                 \
+  --input="${ES_URL}/${ES_INDEX}"           \
+  --output="/data/${ES_INDEX}-mapping.json" \
+  --type mapping
+
+elasticdump                               \
+  --input="${ES_URL}/${ES_INDEX}"         \
+  --output="/data/${ES_INDEX}-data.json"  \
+  --type data
