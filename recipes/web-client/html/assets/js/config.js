a7863c46e2e509964952177ee6b9166eef47b0b9
Anthony Pessy <anthony@cogniteev.com>
2016-04-04 10:50:47 +0200

    Review the navigation of OnCrawl ELK:
     - The previous pages were replaced by 'Crawl Behavior' and 'Exploration Sanity'
     - A new 'SEO impact' was added

--- recipes/web-client/html/assets/js/config.js
+++ recipes/web-client/html/assets/js/config.js
@@ -1,22 +1,14 @@
 var Config = {
     kibana: {
-        url: '/app/kibana'
+       url: '/app/kibana'
     },
     startingDate: undefined,
     endingDate: undefined,
     pages: {
-        'overview/bots-activity': [
+        'crawl-behavior': [
             {
                 row: 1,
-                col: 2,
-                sizex: 3,
-                sizey: 2,
-                title: 'Pages crawled',
-                id: 'Pages-crawled'
-            },
-            {
-                row: 1,
-                col: 5,
+                col: 1,
                 sizex: 4,
                 sizey: 2,
                 title: 'Unique pages crawled',
@@ -24,219 +16,207 @@ var Config = {
             },
             {
                 row: 1,
-                col: 9,
-                sizex: 3,
+                col: 5,
+                sizex: 4,
                 sizey: 2,
-                title: 'Googlebots distribution',
+                title: 'Bots hits',
+                id: 'Pages-crawled'
+            },
+            {
+                row: 1,
+                col: 9,
+                sizex: 4,
+                sizey: 2,
+                title: 'Hits distribution by bot name',
                 id: 'Googlebots-distribution'
             },
             {
                 row: 3,
-                col: 2,
-                sizex: 10,
+                col: 1,
+                sizex: 12,
+                sizey: 2,
+                title: 'Unique pages crawled and bots hits',
+                id: 'Crawl-slash-day'
+            },
+            {
+                row: 5,
+                col: 1,
+                sizex: 12,
+                sizey: 2,
+                title: 'Bots hits by group per day',
+                id: 'Crawl-slash-day-slash-page-group'
+            },
+            {
+                row: 7,
+                col: 1,
+                sizex: 6,
                 sizey: 3,
-                title: 'Top urls crawled (50)',
-                id: 'Top-urls-crawled-(50)'
+                title: 'Top 50 pages crawled',
+                id: 'Top-pages-crawled-(50)'
+            },
+            {
+                row: 7,
+                col: 7,
+                sizex: 6,
+                sizey: 3,
+                title: 'Top 50 resources crawled',
+                id: 'Top-ressources-crawled-(50)'
             }
         ],
-        'overview/pages-performances': [
+        'seo-impact': [
             {
                 row: 1,
-                col: 2,
-                sizex: 5,
-                sizey: 3,
-                title: 'Status distribution',
-                id: 'Status-distribution'
+                col: 1,
+                sizex: 6,
+                sizey: 1,
+                title: 'Active pages',
+                id: 'Active-pages'
             },
             {
                 row: 1,
                 col: 7,
-                sizex: 5,
-                sizey: 3,
-                title: 'Status and Googlebots distribution',
-                id: 'Status-and-Googlebots-distribution'
+                sizex: 6,
+                sizey: 1,
+                title: 'SEO visits',
+                id: 'SEO-visits'
+            },
+            {
+                row: 2,
+                col: 1,
+                sizex: 6,
+                sizey: 2,
+                title: 'SEO visits by group',
+                id: 'SEO-visits-by-group'
+            },
+            {
+                row: 2,
+                col: 7,
+                sizex: 6,
+                sizey: 2,
+                title: 'Active pages by group',
+                id: 'Active-pages-by-group'
             },
             {
                 row: 4,
-                col: 2,
-                sizex: 3,
+                col: 1,
+                sizex: 12,
                 sizey: 2,
+                title: 'SEO visits per day',
+                id: 'SEO-visits-per-day'
+            },
+            {
+                row: 6,
+                col: 1,
+                sizex: 12,
+                sizey: 2,
+                title: 'SEO visits by group per day',
+                id: 'SEO-visits-by-group-per-day'
+            },
+            {
+                row: 8,
+                col: 1,
+                sizex: 12,
+                sizey: 2,
+                title: 'Active pages',
+                id: 'Active-pages-per-day'
+            },
+            {
+                row: 10,
+                col: 1,
+                sizex: 12,
+                sizey: 2,
+                title: 'Active pages by group',
+                id: 'Active-pages-by-group-per-day'
+            },
+            {
+                row: 12,
+                col: 1,
+                sizex: 12,
+                sizey: 3,
+                title: 'Top 50 active pages',
+                id: 'Top-active-pages-(50)'
+            }
+        ],
+        'exploration-sanity': [
+            {
+                row: 1,
+                col: 1,
+                sizex: 4,
+                sizey: 1,
                 title: '200',
                 id: '200'
             },
             {
-                row: 4,
+                row: 1,
                 col: 5,
-                sizex: 3,
-                sizey: 2,
+                sizex: 4,
+                sizey: 1,
+                title: '301',
+                id: '301'
+            },
+            {
+                row: 1,
+                col: 9,
+                sizex: 4,
+                sizey: 1,
                 title: '3xx',
                 id: '3xx'
-             },
+            },
             {
-                row: 4,
-                col: 8,
-                sizex: 2,
-                sizey: 2,
+                row: 2,
+                col: 1,
+                sizex: 4,
+                sizey: 1,
                 title: '404',
                 id: '404'
             },
             {
-                row: 4,
-                col: 10,
-                sizex: 2,
-                sizey: 2,
+                row: 2,
+                col: 5,
+                sizex: 4,
+                sizey: 1,
+                title: '4xx',
+                id: '4xx'
+            },
+            {
+                row: 2,
+                col: 9,
+                sizex: 4,
+                sizey: 1,
                 title: '5xx',
                 id: '5xx'
-             },
-            {
-                row: 6,
-                col: 2,
-                sizex: 10,
-                sizey: 3,
-                title: 'Top pages crawled (50) / status',
-                id: 'Crawled-pages-TOP-50-slash-status'
             },
             {
-                row: 9,
-                col: 2,
-                sizex: 5,
+                row: 3,
+                col: 1,
+                sizex: 6,
                 sizey: 2,
-                title: 'Average bytes',
-                id: 'Average-bytes'
+                title: 'Status codes distribution',
+                id: 'Status-distribution'
             },
             {
-                row: 9,
+                row: 3,
                 col: 7,
-                sizex: 5,
+                sizex: 6,
                 sizey: 2,
-                title: 'Too heavy pages',
-                id: 'Too-heavy-pages'
-             },
-            {
-                row: 11,
-                col: 2,
-                sizex: 10,
-                sizey: 3,
-                title: 'Top heaviest pages (50)',
-                id: 'Top-heaviest-pages'
-            }
-        ],
-        'overview/focus-main-pages': [
-            {
-                row: 1,
-                col: 2,
-                sizex: 4,
-                sizey: 4,
-                title: 'Page group distribution',
-                id: 'Page-group-distribution'
-            },
-            {
-                row: 1,
-                col: 6,
-                sizex: 6,
-                sizey: 4,
-                title: 'Page group crawl distribution',
-                id: 'Page-group-crawl-distribution'
+                title: 'Status codes by bots',
+                id: 'Status-and-Googlebots-distribution'
             },
             {
                 row: 5,
-                col: 2,
-                sizex: 7,
-                sizey: 4,
-                title: 'Page group and status distribution',
-                id: 'Page-group-and-status-distribution'
-            },
-            {
-                row: 5,
-                col: 9,
-                sizex: 3,
-                sizey: 3,
-                title: 'Page group average bytes',
-                id: 'Group-page-average-bytes'
-            }
-        ],
-        'daily-monitoring/crawl': [
-            {
-                row: 1,
-                col: 2,
-                sizex: 6,
-                sizey: 4,
-                title: 'Crawl / day',
-                id: 'Crawl-slash-day'
-            },
-            {
-                row: 1,
-                col: 8,
-                sizex: 4,
-                sizey: 4,
-                title: 'Crawl / day details',
-                id: 'Craw-slash-day-details'
-            },
-            {
-                row: 5,
-                col: 2,
-                sizex: 6,
-                sizey: 4,
-                title: 'Crawl / day / Googlebots',
-                id: 'Crawl-slash-day-slash-Googlebots'
-            },
-            {
-                row: 5,
-                col: 8,
-                sizex: 4,
-                sizey: 4,
-                title: 'Crawl / day / Googlebots details',
-                id: 'Craw-slash-day-slash-Googlebots-details'
-            },
-            {
-                row: 9,
-                col: 2,
-                sizex: 6,
-                sizey: 4,
-                title: 'Crawl / day / page group',
-                id: 'Crawl-slash-day-slash-page-group'
-            },
-            {
-                row: 9,
-                col: 8,
-                sizex: 4,
-                sizey: 4,
-                title: 'Crawl / day / page group details',
-                id: 'Craw-slash-day-slash-page-group-details'
-            }
-        ],
-        'daily-monitoring/pages-performances': [
-            {
-                row: 1,
-                col: 2,
-                sizex: 10,
-                sizey: 4,
-                title: 'Status distribution / day',
-                id: 'Status-distribution-slash-day'
-            },
-            {
-                row: 5,
-                col: 2,
-                sizex: 10,
-                sizey: 4,
-                title: 'Sum of bytes / day',
-                id: 'Sum-of-bytes-slash-day'
-            },
-            {
-                row: 9,
-                col: 2,
-                sizex: 10,
-                sizey: 4,
-                title: 'Sum of bytes / page group / day',
-                id: 'Sum-of-bytes-slash-page-group-slash-day'
-            },
-            {
-                row: 13,
-                col: 2,
-                sizex: 10,
-                sizey: 8,
-                title: 'Status distribution by page group / day',
+                col: 1,
+                sizex: 12,
+                sizey: 7,
+                title: 'Status codes by group per day',
                 id: 'Status-distribution-by-page-group-slash-day'
+            },
+            {
+                row: 12,
+                col: 1,
+                sizex: 12,
+                sizey: 4,
+                title: 'Sum of bytes by group per day',
+                id: 'Sum-of-bytes-slash-page-group-slash-day'
             }
         ]
     }
